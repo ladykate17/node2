890f5da9ba05a2d96c7c003e9c401c8a1c02b1db
ladykate17 <stamm.katherine@gmail.com>
2015-07-21 16:58:41 -0600

    added some notes to ref later

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
 // var fileContents = fs.readFileSync('data.txt'); // Part II code
 
+	// NOTE* readFileSync is Synchronous! The problem with readFileSync is that it is a blocking
+	// method. This means that the server has to wait for the file to be loaded before moving 
+	// on to the next instruction in your code. This is bad! The performance benifits of node
+	// are only realized when slow operations are performed asynchronously so that the server
+	// can continue to handle requests while waiting for the results. This is especially true
+	// in this example since reading from the file system is very slow. In the next part, you
+	// will make your static file server nonblocking.
+
 
 var app = express();
 app.set('view engine', 'jade');
@@ -26,6 +34,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 // 	});
 // });
 
+// NOTE* the golden rule of asynchronous programming: Anything that depends on the result of
+// an asynchronous call must go inside the callback. Code that comes after the asynchronous
+// call is executed before the callback.
+
 // Bonus: Static File Server
 app.get('/:filename', function(req, res) {
 
@@ -33,7 +45,7 @@ app.get('/:filename', function(req, res) {
 		res.header('Content-Type', 'text/html');
 		res.send(data);
 	});
-	
+
 });
 
 var server = app.listen(9795, function() {
