1f7eebe68e7c5d8eeba58cf46c9327351748ebd0
ladykate17 <stamm.katherine@gmail.com>
2015-07-21 16:53:16 -0600

    Bonus complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var fs = require('fs'); // include File System core module --> this provides ope
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
-// var fileContents = fs.readFileSync('data.txt'); // --> Part II code
+// var fileContents = fs.readFileSync('data.txt'); // Part II code
 
 
 var app = express();
@@ -12,18 +12,28 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-// app.get('/', function(req, res){ // --> Part II code
+// Part II code
+// app.get('/', function(req, res){ 
 // 	res.header('Content-Type', 'text/html');
 // 	res.send(fileContents);
 // });
 
 // Part III code
-app.get('/', function(req, res){ 
-	fs.readFile('data.txt', function(err, data){
+// app.get('/', function(req, res){ 
+// 	fs.readFile('data.txt', function(err, data){
+// 		res.header('Content-Type', 'text/html');
+// 		res.send(data);
+// 	});
+// });
+
+// Bonus: Static File Server
+app.get('/:filename', function(req, res) {
+
+	fs.readFile(req.param.filename, function(err, data){
 		res.header('Content-Type', 'text/html');
 		res.send(data);
 	});
+	
 });
 
 var server = app.listen(9795, function() {
