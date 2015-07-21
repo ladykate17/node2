2916e4452f9e7331acb93363ef53499a6a14dbbe
ladykate17 <stamm.katherine@gmail.com>
2015-07-21 16:31:39 -0600

    Part II complete

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var fs = require('fs'); // include File System core module --> this provides ope
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
-var fileContents = fs.readFileSync('data.txt');
+// var fileContents = fs.readFileSync('data.txt'); // --> Part II code
+
 
 var app = express();
 app.set('view engine', 'jade');
@@ -11,10 +12,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function(req, res){
-	res.header('Content-Type', 'text/html');
-	res.send(fileContents);
-})
+
+// app.get('/', function(req, res){ // --> Part II code
+// 	res.header('Content-Type', 'text/html');
+// 	res.send(fileContents);
+// });
+
+// Part III
+app.get('/', function(req, res){ 
+	fs.readFile('data.txt', function(err, data){
+		res.header('Content-Type', 'text/html');
+		res.send(data);
+	});
+});
 
 var server = app.listen(9795, function() {
 	console.log('Express server listening on port ' + server.address().port);
